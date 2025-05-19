302c076381602e78d4711e2fbe5a7bcf924d55cd
Ignacio Chaparro <i.chaparro@uniandes.edu.co>
2025-05-19 17:57:45 -0500

    Revert "pequeño cambio para tener varias explosiones"
    This reverts commit 6189c66cc3cd3bd51d30de1c7c28bd7ed2cb8cac.

--- src/ecs/systems/s_collision_enemy_fireball.py
+++ src/ecs/systems/s_collision_enemy_fireball.py
@@ -22,5 +22,5 @@ def system_collision_enemy_fireball(world: esper.World, pl_special_charge: CSpec
             if ene_rect.colliderect(bull_rect):
                 world.delete_entity(enemy_entity)
                 world.delete_entity(fireball_entity)
-                create_explosion(world, c_t.pos, explosion_info["ExplBomb"])
+                create_explosion(world, c_t.pos, explosion_info)
                 pl_special_charge.add_charge()
